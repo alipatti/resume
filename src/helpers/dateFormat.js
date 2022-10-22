4eaf6450a3c88bd3d9838ba78f10738553666a7b
Alistair Pattison <alistairpattison@gmail.com>
2022-10-22 14:36:53 -0500

    tweaks and gh pages

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,15 +1,35 @@
 module.exports = {
   register: (Handlebars) => {
-    Handlebars.registerHelper("dateFormat", function (dateString) {
-      if (dateString === undefined)
-        return "present"
+    Handlebars.registerHelper("dateFormat", function (date) {
+      if (date === undefined) return null;
 
-      const date = new Date(...dateString.split("-")); // 2009-11-10
-      return date.toLocaleString("default", {
-        month: "long",
-        year: "numeric",
-      });
+      if (date === null) return 'In progress'
+
+
+      if (typeof date == 'object') {
+        // Date range
+        // e.g. [2009-11-10, 2010-11-10]
+        const startDate = dateFormatHelper(date[0]);
+        const endDate = dateFormatHelper(date[1]);
+
+        return `${startDate} - ${endDate}`;
+      }
+
+      // Single date
+      // e.g. 2009-11-10
+      return dateFormatHelper(date);
     });
   },
 };
 
+function dateFormatHelper(dateString) {
+  if (dateString === null)
+    return "present"
+
+  const date = new Date(...dateString.split("-"));
+  const format = {
+    month: "long",
+    year: "numeric",
+  };
+  return date.toLocaleString("default", format);
+}
